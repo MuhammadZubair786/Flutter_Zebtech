950acad43ca26febe3e949aaf9a345c36d414e7d
MuhammadZubair786 <mzhassan444@gmail.com>
2021-12-08 14:59:13 +0500

    first commit

---
+++ FirstClass/first_class.dart
@@ -0,0 +1,100 @@
+void main(){
+  // ; statement terminateor
+  // print keword show output
+  // print("Hello\nWorld");
+  // \t tab use for 5 spaces
+  // \n new line
+  // datatype variablename=value;
+
+  
+  // number(int 1,2,34,45,56,56.89,number=>1,89.78,double =2,3,4,l8.899,1.233.,),String,
+  // String  First_Name = 'zebtech'; "",''
+  
+  // double a=12;
+  // int a1 = 12~/3;  //=>2.4
+  // String a = "true";
+  // print(a.runtimeType); //variable type
+  // int  A_B_1_1 ; //declare
+  // print(A_B_1_1);
+
+  // print(A_B_1_1); //declare
+  // num v1 = 12.12; //declare and inlizalize=>inlization
+  // print(v1);
+  //  true =>boolvariable name
+  String a1= "true"; 
+  print(a1.runtimeType);  //variable type 
+  // int a4_erre;
+  // int a_3 =12;
+  // print(a_3);
+  // int a=12;
+  // double d = 23;
+  // num d2=12;
+    // num  a = 13;
+    // print(a.runtimeType);
+    // define ,call
+    // stdi.read
+    // String v1 = 'dhhd';
+
+//     res,user
+// false => a =>true=>
+   
+    // bool a=false;
+    //  print(a);
+    // a=true; 
+    // List =>Array
+    // Array =>collection Of elements
+    // Homogenous  =>Same data type
+    // Hetegenous=>Dart
+    // List data = [1,2,3,4,5];
+    // // 0 index=>0
+    // data[0]="asad";
+    // print(data);
+    // List data2 = ["asad","usama","omer",10,true];
+    // index no
+    // key,value=>Map
+    // var data1 = {name:"asad",2:"sjjs"};
+ 
+    // data[3] ="ghggsg";
+    // print(data);
+    // Hetergenous = >different Datatype
+  //  String a= "asad";
+  //  String b = "saad";
+  //  String a1= "asad";
+  //  String b1 = "saad";
+  //  String a2= "asad";
+  //  String b2 = "saad";
+  // List a = [1,"asad","omer",true,12.45];
+  // index [0]
+  // list store => index
+  // access data =>listname [index]
+  // =>index start 0
+  // print(a[1]);
+  // Map data = {name : "asad"}
+  /* String fisrtname = "asad";
+  int age =10;
+   num height = 6.6;
+   print("FisrT name : ${fisrtname}\nAge : ${age}");
+  //   print("data Type Of First name : ${fisrtname.runtimeType}\n fafaffafafa   of Age : ${age.runtimeType}");
+  // print(age);*/
+  // single,Multiline
+  // Single Comment
+
+  // List data =[1,22,3];   //list make
+  // print("Element Of index 0 : ${data[0]}\nElement Of index 1 : ${data[1]}");
+
+
+// int 1a
+// var a b=0
+// double #1ab c1=123.45;
+
+// int by = 2000;
+// print("Brith year : ${by}");  //concatenation
+//comments can make code
+//readable
+
+String a="ggdgdgd";
+print("a ortdfgfvgv 5 jcchf");
+
+ 
+
+}
