97e3131add64d3dace1c72b21fd6a8f94cfc2f8b
MuhammadZubair786 <mzhassan444@gmail.com>
2021-12-08 15:59:32 +0500

    Class_4

---
+++ main.dart
@@ -0,0 +1,46 @@
+void main(){
+  // String Method
+//  String data = 12.45;  //Smit hyderabad  =>
+  // String d2 = "smit";
+  //index 
+  // print(data.toLowerCase());
+//   print(data.toUpperCase());
+//   // print(data[5]);
+//  var v2 = data.split(' ');
+//  print(v2);
+//  String v3 = data.substring(8);
+//  print(v3);
+// String v3 = data[0].toUpperCase()+data.substring(1).toLowerCase();
+// print(v3);
+// int d = data.length;
+// print(data[3]);
+// bool st = data.startsWith('sF');  //true,false
+// print(st);
+// bool st = data.endsWith('m');
+// print(st);
+
+// String data = "SMIT";  //Smit hyderabad
+// String d2 = "SMhT"; 
+// print(d2.compareTo(data));   //0,1,-1
+
+// first value greater => 1
+// first value smaller => -1
+// first and second equal =0
+//  String data = "SMItFGRTIGI";  //Smit hyderabad
+// int b = data.lastIndexOf('I');
+// print(b);
+// A ==>65=>90 Cpital 
+//a=>97=>121  Small
+// C=0=>return 
+// c=0,
+
+// String data = 'AbBTE';
+// int d = data.codeUnitAt(0);
+// int d2= data.codeUnitAt(2);
+// print(d);
+
+
+
+
+
+}
