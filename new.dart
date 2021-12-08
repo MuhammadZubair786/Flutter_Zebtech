86716dba975f5e043e6db3b50a8426dc1c3952a9
MuhammadZubair786 <mzhassan444@gmail.com>
2021-12-08 15:07:33 +0500

    added files

---
+++ new.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+
+void main() {
+  // stdout.write("Enter Vistiors Name :");
+  // var name =  stdin.readLineSync();
+
+  // stdout.write("Enter PRODUCT Title :");
+  // var prod_title =  stdin.readLineSync();
+
+  // stdout.write("Enter Quantity  :");
+  // int? data =  int.parse(stdin.readLineSync()!);
+
+  // print(" ${name} ordered ${data} ${prod_title} on XYZ Clothing store");
+  // var name =  stdin.readLineSync();
+  // var data = int.tryParse(name ?? "");
+  // print(data);
+  // stdout.write("Enter Number 1 :");
+  // int n1 = int.parse(stdin.readLineSync()!);
+
+  int tk = 600;
+  int qty = int.parse(stdin.readLineSync()!);
+  print("Price : ${tk*qty}");
+
+  
+
+  // stdout.write("Enter  :");
+  // int n2 = int.parse(stdin.readLineSync()!);  //input
+
+  // double F = (n2* 9 / 5) + 32;
+  // print(F);
+}
---
+++ today.dart
@@ -0,0 +1,67 @@
+void main(){
+    //method=>class(blueprint,template)=>fun=>method
+    // fun=>fun
+    // class Animal=>
+    // method=>dog=>
+    // cat =>
+    // oop(object Oriented Programming)
+    // class=>inheritance,
+    String fnname="aSfDf5dEeshhsha";
+    // method
+  // fnmame.compareTo(other)
+  // print(fnname.length); //4
+  // print(fnname.startsWith("as"));
+  // print(fnname.indexOf("a",1)); //first index 
+  // php=>variable ,function Data()
+  // function(code reusable)=>userdefined or predefined=>language 
+    //func =>func
+    // print(fnname.replaceFirst("d", "e"));
+  //  print( fnname.replaceFirst("e","z"));
+    // print(fnname.replaceAll("d", "e"));
+  //  print(fnname.indexOf("s",2));
+  // print(fnname.replaceRange(0, 6, "smit"));
+  // print(fnname.toUpperCase());
+  // title case
+   String name="sMIt gaider";
+  //  String name2 = "sMIt iyderabad";
+  //  print(name[0].toUpperCase()+name[1].toLowerCase()+name[2].toLowerCase()+name[3].toLowerCase());
+// print();
+// var b = name.indexOf(" "); //4
+
+// // print();
+// var b1 =name[0].toUpperCase()+
+// name.substring(1,5).toLowerCase()+
+// name[b+1].toUpperCase()+
+// name.substring(b+2).toLowerCase();
+// print(b1);
+// print(name.split("d"));//string change in list
+// print(name.compareTo(name2)); //0 ,1,-1
+// print(name.codeUnitAt(5)); //code =>ascii value
+// print(name.codeUnits); //all string ascii value
+// print(name.contains("H"));
+
+// int a= 12;
+
+// // pre,post =>increment,decrement
+// //++varname
+// //varname ++
+// print(a++);
+// print(a);
+
+// int a=10;
+// int b = --a - a - a-- - --a;
+        // 9 + 9 +  9 + 7
+
+        //  11 + 11 + 11 + 13;
+// print(b);
+// print(a);
+
+String a = "asadadagagdf.cmkjvhdjkf .skhdfhsdf  jjjdjd.";
+print(a.split("."));
+
+
+
+
+   
+   
+}
