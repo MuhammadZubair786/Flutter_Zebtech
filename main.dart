0dddb76fcd24f59d6ef47679f958336dc31baaae
MuhammadZubair786 <mzhassan444@gmail.com>
2021-12-08 16:12:15 +0500

    added new Class

---
+++ main.dart
@@ -0,0 +1,68 @@
+import 'dart:io';
+
+void main() {
+  // int a=10;
+  // if,if else =>keyword
+  // if,else if ,else
+  // call();
+  //const  pi=3.14;
+
+  // int a= 0;
+  // if(a>0){
+  //   print("Positive Number");
+  // }
+  // else if(a<0){
+  //   print("Negative Number");
+  // }
+  // else{
+  //   print("Zero");
+  // }
+  // 2 number,+,-,*,/,%
+  // int v1 = int.parse(stdin.readLineSync()!);
+  // int v2 = int.parse(stdin.readLineSync()!);
+
+  // var d= stdin.readLineSync();
+  // // print(d.runtimeType);
+  // if(d=="+" ){
+  //   int c = v1+v2;
+  //   print(c);
+
+  // }
+  // else  if(d=="/"){
+  //  int c = v1~/v2;
+  //   print(c);
+
+  // }
+  //   else  if(d=="-"){
+  //  int c = v1-v2;
+  //   print(c);
+  // }
+  // else{
+  //   print("Incorrect Operator");
+  // }
+  // String d = stdin.readLineSync()!.toLowerCase();
+
+  // if (d == "RED") {
+  //   print(" Must Stop");
+  // } else if (d == "YELLOW") {
+  //   print("Ready");
+  // } else if (d == "green") {
+  //   print("Go");
+  // } else {
+  //   print("Incorrect Option");
+  // }
+
+  // AsCII CODE (A,Z)//upper case(65,90),lower case(97-122) ,number(49-57),special symbol
+  // String v1 = stdin.readLineSync()!;
+  // if(v1.length==1){
+  // if(v1.codeUnitAt(0)>=65 && v1.codeUnitAt(0)<=90){
+  //   print("Capital");
+
+  // }
+  // }
+  // else{
+  //   print("Enter Only One Letter ");
+  // }
+  
+
+}
