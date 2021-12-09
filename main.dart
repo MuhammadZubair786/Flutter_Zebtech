fa1f2b629c61bb9a2dca94d1b1b4a7f758417d8a
MuhammadZubair786 <mzhassan444@gmail.com>
2021-12-10 03:55:24 +0500

    Class_11

---
+++ main.dart
@@ -0,0 +1,77 @@
+void main() {
+  // var i =10;
+  // for(;i>0;i--){
+  //   print(i);
+
+  // }
+  // 0=>100=>
+  // even=[],odd=[]
+  // List even = [], odd = [];
+
+  // for (var i = 0; i <= 100; i++) {
+  //   if (i % 2 == 0) {
+  //     // print("Even Number :$i");
+  //     even.add(i);
+  //   } else {
+  //     // print("Odd Number :$i");
+  //     odd.add(i);
+  //   }
+  // }
+  // print("Even List : $even");
+  // print("Odd List : $odd");
+
+  // List a1 = [1, 2, 3, 4, 5, 12, 6, 6, 7, 8, 989, 67, 90, 45, 32, 98, 89, 45];
+  // List even = [];
+  // print(a1.length);
+  // for (var i = 0; i < a1.length; i++) {
+  //   //0,2,3
+  //   // print(i);
+  //   // print("index : $i value ${a1[i]}");
+  //   if (a1[i] % 2 == 0) {
+  //     even.add(a1[i]);
+  //   }
+  // }
+  // print(even);
+
+  // List a1 = [1,3,5,7,9,11,13];
+  // for(var i =0;i<a1.length;i++){
+  //  if(true){
+  //    print("Odd number : ${a1[i]}");
+  //  }
+  // }
+
+  // // List a1=[1,];
+  // List a1 = ["asad","omer","asad","baber","asad"];
+  // // obj=>[0:{status:"fail"}]
+  // for(var i =0;i<a1.length;i++){
+  //   if(a1[i]=="asad"){
+  //     print(i);
+  //   }
+  // }
+
+//  List a=[1,2,3,4,5,6,7,8];
+
+//  List even=[];
+ 
+//  for(var i =0;i<a.length;i++){
+// // a[7]=8
+// // 8%2==0
+//    if(a[i]%2==0){
+   
+//      even.add(a[i]); 
+   
+//    }
+//    //[]
+//    //,2
+//    //,2,
+//    //2,4
+//    //2,4
+//    //2,4,6
+//    //2,4,6
+//    //2,4,6,8
+
+//    print(even);
+//  }
+   //19 
+
+}
