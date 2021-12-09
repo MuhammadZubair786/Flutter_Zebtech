f0073cccfdc289c22c903b84a30e586e0f30394f
MuhammadZubair786 <mzhassan444@gmail.com>
2021-12-10 03:42:55 +0500

    class 8

---
+++ main.dart
@@ -0,0 +1,77 @@
+void main() {
+//   List a1 = [1,63632,7373,7373,89,7782];
+//  a1.sort();
+//   print(a1);
+//   print(List.of(a1.reversed));
+// key,value
+  // var a1 = {1:"sjjs", 2: "Omer", 3: "Baber", 4: "Usman"};
+  // asad ;las
+  // print(a1.runtimeType);
+  // print(a1.keys.toList());
+  // print(a1.values);
+  // a1[3]= "asad";
+  // print(a1);
+  // null safety
+  var a2 = {1: 'asad', 1: "omer", 1: "ali", 1: true, 1: 23.90};
+  // a2.putIfAbsent(3,()=>"baber");
+  // print(a2["4"]); //a[key]
+  // a2.putIfAbsent('key', () => "Zebtech");  //callback(function,method,function)
+//  a2[20]="Zebtech";
+//  print(a2);
+// a2.update("2", (value) => "data");
+  // a2[2]="zebtech";/
+  // a2.updateAll((k, v) => 2);
+
+  // print(a2);
+  // // print(a1.length);
+  // a2.putIfAbsent(3, () => "asad");
+  // print(a2);
+  //  var b = {"name" : "asad","class":12};
+
+  // var b2 = {"course":"FLUTTER","educ":"Fg"};
+
+  //  var b3 = {"course1":"FLUTTER Andriod ","educ":"GSGGS","school":"Zebtech"};
+
+// // spread Operator ...
+//   var c = {}..addAll(b)..addAll(b2)..addAll(b3);
+//   print(c);
+
+// var c = {...b,...b2,...b3,};
+// print(b);
+// print(b2);
+// print(c);
+// var b1 = {1:"asad",2:"omer",3:"smit"};
+
+// var b1 = {"name" : true , 2: "asad" , 3: true,"Course":"Smit"};
+// b1.remove(2); // remove(key)
+// print(b1);
+
+// b1.removeWhere((k,v)=> v == "zebtech");   
+// b1.removeWhere((k,v)=> v == "faith");   
+// b1.removeWhere((k,v)=> v == "hiast");   
+
+// print(b1);
+// bool b = b1.containsValue("Smit");
+// print(b); 
+// if(b==false){
+//   b1["Course"]= "Zebtech";
+
+// }
+// else{
+//   print("Sorry Already Give To smit");
+// }
+// print(b1);
+// var b = "sjjjsjs";
+// print(b[0]);
+// List  a1 = ["shsh",];==>0
+// var b1 = {"name" : tr
+// ue , 2: "asad" , 3: true,"Course":"Smit"};
+// var b = b1.toString();
+// print(b[5]);
+// b1.forEach((key, value) {
+//   print("$key = >$value");
+// });
+// var data=null;
+
+}
+
