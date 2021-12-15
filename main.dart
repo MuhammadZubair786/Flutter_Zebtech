e6888a1b22bd613da6bf2c1e7d6587251a0bdd4b
MuhammadZubair786 <mzhassan444@gmail.com>
2021-12-15 18:51:05 +0500

    Added

---
+++ main.dart
@@ -0,0 +1,99 @@
+
+void main() {
+
+  // enum==>Mistake
+  // var b = "sunday";
+  // if(b=="Sunday"){
+  //   print("Holiday");
+  // }
+  // else{
+  //   print("Working Day");
+  // }
+
+// enumname  varibale = enum.variable
+  // Days days= Days.Sunday;
+  // print(days);
+
+  // if(days==Days.Sunday){
+  //   print("Holiday");
+
+  // }
+  // else{
+  //   print("Working day");
+
+  // }
+
+  // good morning,
+
+  // // Gender g = Gender.Mr;
+  // Day d1 = Day.Evening;
+  // String msg = "Hello";
+  // var b ="2";
+  // b  ="1";
+  // print(b);
+  // v++;
+  // v+=1 v=v+1
+  // v=v+1
+  // String g = "Mr";
+  // g=g[0].toUpperCase()+g.substring(1).toLowerCase();
+  //  switch(g){
+  //   case "Mr":
+  //   msg =" Mr Asad"; // Mr Asad
+  //   break;
+  //    case "Mrs":
+  //   msg =" Mrs Ayesha";
+  //   break;
+  // }
+
+  // switch(d1){
+  //   case Day.Morning:
+  //   msg=msg + "Good Morning";
+  //   break;
+
+  //   case Day.Afternoon:
+  //   msg = msg + "Afternoon";
+  //   break;
+
+  //    case Day.Evening:
+  //   msg  = msg + "Good Evening"; // +=  msg =msg+"Good Eveneing"
+  //   break;
+  //   default:
+  //   print("Default");
+
+  // }
+  // print(msg);
+
+  // set => same data,collection of element/data,unique
+
+  // ignore: equal_elements_in_set
+  var b = {1,3,5,2,4}; //list to set()//last ko captaure
+  // print(b);
+  var c = {1,8,6,8};
+  // print(b.intersection(c));  //common
+  // print(c.union(b));
+
+  // print(c.difference(b));  //3,5,2,1,6,8
+  // print(b.lookup(1));
+  // b.fold(0, (previousValue, element) => print(previousValue));
+
+  // function ==> reuse the code
+
+
+
+
+
+}
+//   enum Gender{
+//   Mr,Mrs
+// }
+
+// enum Days{
+//   Sunday,
+//   monday
+// }
+
+// enum Day{
+//   Morning,
+//   Afternoon,
+//   Evening
+// }
