16e06a2892dc951afebbe76aa35360092570ec4e
MuhammadZubair786 <mzhassan444@gmail.com>
2021-12-16 01:12:19 +0500

    added files

---
+++ main.dart
@@ -0,0 +1,47 @@
+void main() {
+  // var obj = {"fname": "asad", "lanem": 23, "3": "Ali",4:"1",5:"asad","Course":"smit"};
+  // print(obj.runtimeType);//=>object
+  // obj[4]=43; //obje[key]=value;
+  // bool b1 = obj.containsKey("course");
+  // if (b1 == true) {
+  //   print("Already Register For Free Courses");
+  // } else {
+  //   obj["course"] = "Zebtech";
+  // }
+
+// [12,4,56,78];
+//   var obj = {
+//     "name1": "asad",
+//     "name2": "omer",
+//     "name3": "ali",
+//     "name4": "azmat"
+//   };
+
+  // obj.removeWhere((key, value) => `key`.startsWith("name"));
+  // print(obj);
+
+  // obj.forEach((key, value) { 
+  //   print("Key => $key , Value $value");
+  // });
+
+  // List l1= [1,2,3,4,"Name"];
+  List l2=["Smit","Zebtech","Hist","faith",23];
+
+  // var obj = Map.fromIterables(l1, l2);
+  // print(obj); //Lists Change Into Map =>dynamic,dynamic
+
+  List v1 = [
+    {"name":"asad","course":["It","Cs","Andriod"],"educ":13},
+    {"name":"ome","course":["It","Cs","Andriod"],"educ":10000}
+  ];
+  // print($course[1]); // andriod
+  // print();
+  // print(v1[0]["course"][1]);
+
+  //loop=> Repeation  
+  // for,while,do while
+  // for,while=>pre condition check;
+  // do while =>post condition check;
+  // for,=>run
+  // while=>depend on use
+}
