e453990aaf6cb03fe65278be82d29d6a69d5bdc1
MuhammadZubair786 <mzhassan444@gmail.com>
2021-12-15 18:48:59 +0500

    Class_17

---
+++ main.dart
@@ -0,0 +1,29 @@
+void main() {
+  // var i = 0;
+  // do {
+  //   print(i);
+  //   i++;
+  // } while (i < 10);
+
+
+// Break,Continue
+var i=0;
+while(i<=10){
+ 
+  if(i%2==5){
+    print(i);
+    break;
+   
+  }
+  else{
+    print(i);
+
+  
+  }
+  i++;
+
+}
+
+
+
+}
