b498a9d3a3d1e800ada77598084c3cdddc420689
MuhammadZubair786 <mzhassan444@gmail.com>
2021-12-16 01:06:45 +0500

    added files

---
+++ main.dart
@@ -0,0 +1,67 @@
+void main() {
+  //  var i = 0 ;
+  // do{
+  //   print(i);
+  //   i++;
+  // }
+  // while(i>10);
+
+//   for(var i=0;i<=10;i++){
+//     // oddnumber
+//   if(i%5!=0){
+//     // continue; //skip again loop increment loop ready for run
+//     break;
+//   }
+//   print(i);
+// }
+// var i=0;
+// while(i<=10){
+//   if(i==5){
+//    print("5 ,$i");
+//     i++; //i=5
+//     continue;
+//   }
+//   print(i);
+//   i++;
+// }
+
+// Switch case   =>if else if ,else
+  // var i = 20;
+  // var c=1;
+  // var a=2;
+  // var b=4;
+  // 0 to 7
+
+  // switch (i) {
+  //   case 20:
+  //     print("Hello");
+  //     break;
+
+  //   case 2:
+  //     print("Hello 2");
+  //     break;
+
+  //   case 3:
+  //     print("Done");
+  //     break;
+
+  //   default:
+  //     print("Default");
+  // }
+
+  int roll_no=36;
+  switch(roll_no){
+    case 1:
+    print("Roll no 1");
+    break;
+     case 2:
+    print("Roll no 2");
+    break;
+     case 3:
+    print("Roll no 3");
+    break;
+    default:
+    print("Hello");
+  }
+
+}
