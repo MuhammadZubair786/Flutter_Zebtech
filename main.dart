c3b9bd8e38654f7e2a986622e5c3f610a8c3c7e5
MuhammadZubair786 <mzhassan444@gmail.com>
2021-12-25 22:35:00 +0500

    new files add'

---
+++ main.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+void main(ar){
+  // print(ar[0]); //Arrugmnet List=>first
+  // var a = File(ar[2]); //a.csv
+  // print(ar[1]);
+  // a.readAsLinesSync();
+  // print(a.readAsLinesSync());
+  // a.writeAsStringSync("\n Hello"+a.readAsStringSync());
+  // print("\n 1 Data Show");
+  // print("\n 2 Write");
+  // var inp = stdin.readLineSync();
+  // if(inp =="1"){
+  //      print(a.readAsStringSync());
+  // }
+  // else if(inp=="2"){
+  //   String b1 = stdin.readLineSync()!;
+  //   a.writeAsStringSync(a.readAsStringSync()+b1);
+    
+  // }
+
+
+
+  
+
+}
