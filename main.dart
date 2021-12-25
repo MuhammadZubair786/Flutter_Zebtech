3673c906e04d9b141b90565b8b3be9202af60e25
MuhammadZubair786 <mzhassan444@gmail.com>
2021-12-25 22:39:21 +0500

    Class_@6

---
+++ main.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+
+class Car {
+  String color = "red"; //instance Member
+  String wheel = "4";
+
+ List PrintData() {
+  return [1,2,3,4,5];
+  }
+
+  // int data() {
+  //   return wheel;
+  // }
+}
+
+void main() {
+  Car a = Car();
+  print(a.color);
+  // print(a.wheel);
+  print(a.PrintData());
+  // print(a.data());
+  // a.PrintData();
+}
+// random.nextInt()
+// (a.first)
+// .substring()
+// a=10;
+// a[0]
+
+// PrintHandler
+// Method = > method use for in class=>func =>.method() 
+// Function = >,function,call ka bhair => function
+
+// class Car{
+
+
+//   int hello(){
+//     return 10;
+
+//   }
+  
+// }
+// Car.hello();
+
+// class Car{
+//     String color ="red"; //instance Member
+// }
+
+
+
+
+
+
